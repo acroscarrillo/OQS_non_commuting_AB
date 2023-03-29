3e19fe857da4d4dc7a06ef687d48db9cd79b0ddd
Alex's mac <a.croscarrillo@gmail.com>
2023-03-29 16:36:29 +0100

    minor fix

--- src/main.jl
+++ src/main.jl
@@ -1,6 +1,7 @@
 using LinearAlgebra
 using LinearSolve
 
+
 function A(L::Int, p::Real)
     temp = zeros(Float64,(L,L))
     for col = 1:L
@@ -11,15 +12,15 @@ function A(L::Int, p::Real)
     return temp * transpose(temp)
 end 
 
-
 function M(h,A,B)
     h_dim, A_dim, B_dim = size(h)[1], size(A)[1], size(B)[1]
     if !(h_dim == A_dim == B_dim)
         throw(ArgumentError("Dimensions of h, A and B mismatch"))
     end
-    left = I(h_dim)-im*h-0.5*transpose(A-B)
-    right = I(h_dim)+im*h-0.5*(A-B)
-    return kron(left,right)
+    Id = I(h_dim)
+    left = Id - im*h - 0.5*transpose( A - B )
+    right = Id + im*h - 0.5*( A - B )
+    return kron(left, Id) + kron(Id, right)
 end
 
 function NESS(h,A,B)
@@ -27,16 +28,3 @@ function NESS(h,A,B)
     sol = solve(prob)
     return reshape(sol.u, size(h))
 end
-
-# aux_mat = np.zeros((L,L),dtype=np.complex128)
-#         for i in range(L): 
-#             for j in range(L):
-#                 if j+i<=L-1:
-#                     aux_mat[j,j+i]= ( np.random.normal(0,1) + 1j*np.random.normal(0,1) )/(i+1)**b#np.exp(i*b/L)#(i+1)**b#(i/L+1)**b
-#                     aux_mat[j+i,j]= ( np.random.normal(0,1) + 1j*np.random.normal(0,1) )/(i+1)**b#np.exp(i*b/L)#(i+1)**b#(i/L+1)**b
-#     #                 aux_mat[j,j+i]= np.random.normal(0,1) #ATENTION!!! MATRIX "A" IS RESTRICTED TO REAL ENTRIES, CHANGE THIS IF NEEDED!!! 
-#         A = np.dot(aux_mat, aux_mat.conj().T)
-
-#         lamb = LA.eigvalsh(A)
-
-#         return A/(2*max(lamb))
