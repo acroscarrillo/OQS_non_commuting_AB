b59c9f42f17081718f90a5c2a8921cef216e3566
Alex's mac <a.croscarrillo@gmail.com>
2023-06-30 09:43:51 +0100

    morning!

---
+++ data_gen/negativity/neg_L_A_half_vs_L_zoom.jl
@@ -0,0 +1,31 @@
+include("../../src/src.jl")
+
+using ProgressBars
+using Statistics
+using CSV 
+
+L_array = Vector(400:400:2000)
+p_array = Vector(0.90:0.01:1.1)
+runs_array = Vector(1000:-200:200)
+data_array = zeros( length(L_array)*length(p_array), 5 ) # MI, MI_err, L, p, L_A
+
+counter = 1
+for (_,p) in ProgressBar(enumerate(p_array))
+    for (i, L) in enumerate(L_array)
+        subsys_A = Vector(1:L÷2)
+        subsys_B = Vector(L÷2+1:L)
+        temp = zeros(runs_array[i])
+        for j=1:runs_array[i]
+            A = pwr_law_mat(L,p)
+            # B = pwr_law_mat(L,p)
+            B = I(L)-A
+            C = correlation_ness(A, B)
+            temp[j] = negativity(C,subsys_A,subsys_B)
+        end
+        data_array[counter,:] .= mean(temp), std(temp)/sqrt(runs_array[i]), L, p, L÷2
+        counter += 1
+    end
+end
+
+df_wb = DataFrame(data_array, ["neg","neg_err","L","p","L_A"])
+CSV.write("data/neg_at_L_A_half_zoom.csv", df_wb)
---
+++ data_plotting/neg_vs_p.jl
@@ -0,0 +1,18 @@
+include("../src/src.jl")
+
+df = DataFrame(CSV.File("data/neg_at_L_A_half.csv"))
+
+p_array = unique(df.p)
+L_array = unique(df.L)
+neg, neg_err, p = reshape([], 0, length(p_array)),reshape([], 0, length(p_array)),reshape([], 0, length(p_array))
+for val in L_array
+    df_subset = df[df.L .== val,:]
+    temp_neg, temp_neg_err, temp_p = transpose(df_subset.neg), transpose(df_subset.neg_err), transpose(df_subset.p)
+    neg, neg_err, p = vcat(neg,temp_neg), vcat(neg_err,temp_neg_err), vcat(p,temp_p)
+end
+
+plot(transpose(float.(p)), transpose(float.(neg)), yerr = transpose(float.(neg_err)),
+        labels="L = ".*string.(transpose(L_array)), xlabel="p", ylabel="neg",dpi=300)
+
+savefig("figs/neg_vs_p/neg_vs_p_nb.pdf")
+savefig("figs/neg_vs_p/neg_vs_p_nb.png")
