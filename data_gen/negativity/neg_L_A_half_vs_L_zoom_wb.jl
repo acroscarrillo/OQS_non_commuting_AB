a9c9538972c3aa33855a94e07fcb5fac18dab269
Alex's mac <a.croscarrillo@gmail.com>
2023-07-03 13:38:19 +0100

    data data patata

--- data_gen/negativity/neg_L_A_half_vs_L_zoom_wb.jl
+++ data_gen/negativity/neg_L_A_half_vs_L_zoom_wb.jl
@@ -11,16 +11,18 @@ data_array = zeros( length(L_array)*length(p_array), 5 ) # MI, MI_err, L, p, L_A
 
 counter = 1
 for (_,p) in ProgressBar(enumerate(p_array))
-    for (i, L) in enumerate(L_array)
-        subsys_A = Vector(1:L÷2)
-        subsys_B = Vector(L÷2+1:L)
+    for (i, L) in ProgressBar(enumerate(L_array))
+        subsys_A, subsys_B = Vector(1:L÷3), Vector(2*L÷3+1:L)
         temp = zeros(runs_array[i])
         for j=1:runs_array[i]
             A = pwr_law_mat(L,p)
             # B = pwr_law_mat(L,p)
             B = I(L)-A
             C = correlation_ness(A, B)
-            temp[j] = negativity(C,subsys_A,subsys_B)
+            C_sub = sub_correlation(C, vcat(subsys_A,subsys_B))
+            L_AB = length(vcat(subsys_A,subsys_B))
+            subsys_A, subsys_B = Vector(1:L÷3), Vector(L÷3+1:L_AB) # due to the trace
+            temp[j] = negativity(C_sub,subsys_A,subsys_B)
         end
         data_array[counter,:] .= mean(temp), std(temp)/sqrt(runs_array[i]), L, p, L÷2
         counter += 1
@@ -28,4 +30,4 @@ for (_,p) in ProgressBar(enumerate(p_array))
 end
 
 df_wb = DataFrame(data_array, ["neg","neg_err","L","p","L_A"])
-CSV.write("data/neg_at_L_A_half_zoom.csv", df_wb)
+CSV.write("data/neg_at_L_A_half_zoom_wb.csv", df_wb)
