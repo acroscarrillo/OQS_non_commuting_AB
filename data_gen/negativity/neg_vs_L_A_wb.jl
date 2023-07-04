b629ad3d89d523d610756c8a2e93e971bf8debec
Alex's mac <a.croscarrillo@gmail.com>
2023-07-04 16:02:07 +0100

    some tweaks

--- data_gen/negativity/neg_vs_L_A_wb.jl
+++ data_gen/negativity/neg_vs_L_A_wb.jl
@@ -4,10 +4,10 @@ using ProgressBars
 using Statistics
 using CSV 
 
-L = 1000
-L_A_array = Vector(13:20:L÷3)
+L = 100
+L_A_array = Vector(13:3:L÷3)
 p_array = [0.0, 0.2, 0.6, 1.0, 1.6]
-runs = 10
+runs = 1000
 data_array = zeros( length(L_A_array)*length(p_array), 5 ) # MI, MI_err, L, p, L_A
 
 counter = 1
@@ -15,23 +15,20 @@ for (_,p) in ProgressBar(enumerate(p_array))
     for (_, L_A) in ProgressBar(enumerate(L_A_array))
         subsys_A,subsys_B = Vector(1:L_A), Vector(L_A+L÷3:L)
         temp = zeros(runs)
-        for j=1:runs
+        for j=1:runs 
             A = pwr_law_mat(L,p)
             # B = pwr_law_mat(L,p)
             B = I(L)-A
             C = correlation_ness(A, B)
-            C_sub = sub_correlation(C, vcat(subsys_A,subsys_B))
-            L_AB = length(vcat(subsys_A,subsys_B))
-            subsys_A, subsys_B = Vector(1:L÷3), Vector(L÷3+1:L_AB) # due to the trace
+            C_sub = sub_correlation(C, vcat(subsys_A,subsys_B)) # trace boundary out
+            L_AB = length(vcat(subsys_A,subsys_B)) # new effective L after trace
+            subsys_A, subsys_B = Vector(1:L_A), Vector(L_A+1:L_AB) # due to the trace
             temp[j] = negativity(C_sub,subsys_A,subsys_B)
         end
         data_array[counter,:] .= mean(temp), std(temp)/sqrt(runs), L, p, L_A
         counter += 1
     end
 end
-
-
-
 
 df_wb = DataFrame(data_array, ["neg","neg_err","L","p","L_A"])
 CSV.write("data/neg_vs_L_A_wb.csv", df_wb)
